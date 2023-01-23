a099e92f0a17d8225f6bc8a0186febb50a1c8280
vriskaserket51 <serketvriska51@gmail.com>
2023-01-23 16:27:55 +0900

    Renamed function name

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { ModelBase } from "../router/RouterBase";
+import { ModelBase } from "../router";
 import {
     verifyAccessTokenMiddleware,
     verifyRefreshTokenMiddleware,
